a2d25cfc684987ced845a0f090d4ef9efd71889d
Tati Micheletti <tati.micheletti@gmail.com>
2019-07-18 20:49:32 +0000

    updated functions to usefun

--- R/makeReclassifyMatrix.R
+++
@@ -1,5 +0,0 @@
-makeReclassifyMatrix <- function(table, originalCol, reclassifiedTo){
-  # Matrix with 3 columnd: from X to Y reclass to Z
-  m <- matrix(data = c(table[[originalCol]], table[[originalCol]], table[[reclassifiedTo]]), ncol = 3, byrow = FALSE)
-  return(m)
-}
--- R/reclassifyRastersDirect.R
+++
@@ -1,6 +0,0 @@
-reclassifyRastersDirect <- function(rasterToReclassify, table, originalCol, reclassifiedTo){
-  browser()
-  m <- matrix(data = c(table[[originalCol]], table[[reclassifiedTo]]), ncol = 3, byrow = FALSE)
-  
-}
-  
